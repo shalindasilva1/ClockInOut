87f08185445a88e2ee76b730382d18267ecfb638
Shalinda Silva <shalindasilva1@gmail.com>
2025-01-22 11:28:16 +0100

    Comments and actionresults

--- UserAPI/Controllers/UsersController.cs
+++ UserAPI/Controllers/UsersController.cs
@@ -5,10 +5,17 @@ using UserAPI.Services;
 
 namespace UserAPI.Controllers;
 
+/// <summary>
+/// UsersController handles user-related operations such as retrieving, creating, updating, and deleting users.
+/// </summary>
 [ApiController]
 [Route("[controller]")]
 public class UsersController(IUserService userService) : Controller
 {
+    /// <summary>
+    /// Retrieves all users.
+    /// </summary>
+    /// <returns>A list of users.</returns>
     [Authorize]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<UserDto>>> GetUsers()
@@ -16,7 +23,12 @@ public class UsersController(IUserService userService) : Controller
         var users = await userService.GetAllUsersAsync();
         return Ok(users);
     }
-    
+
+    /// <summary>
+    /// Retrieves a user by ID.
+    /// </summary>
+    /// <param name="id">The ID of the user to retrieve.</param>
+    /// <returns>The user with the specified ID.</returns>
     [Authorize]
     [HttpGet("{id}")]
     public async Task<ActionResult<UserDto>> GetUser(int id)
@@ -24,14 +36,19 @@ public class UsersController(IUserService userService) : Controller
         try
         {
             var user = await userService.GetUserByIdAsync(id);
-            return user;
+            return Ok(user);
         }
         catch (Exception e)
         {
             return BadRequest(e.Message);
         }
     }
-    
+
+    /// <summary>
+    /// Retrieves a user by username.
+    /// </summary>
+    /// <param name="username">The username of the user to retrieve.</param>
+    /// <returns>The user with the specified username.</returns>
     [Authorize]
     [HttpGet("username/{username}")]
     public async Task<ActionResult<UserDto>> GetUserByUsername(string username)
@@ -39,14 +56,19 @@ public class UsersController(IUserService userService) : Controller
         try
         {
             var user = await userService.GetUserByUsernameAsync(username);
-            return user;
+            return Ok(user);
         }
         catch (Exception e)
         {
             return BadRequest(e.Message);
         }
     }
-    
+
+    /// <summary>
+    /// Creates a new user.
+    /// </summary>
+    /// <param name="user">The user data to create.</param>
+    /// <returns>The created user.</returns>
     [Authorize]
     [HttpPost]
     public async Task<ActionResult<UserDto>> PostUser(UserDtoCreate user)
@@ -65,7 +87,13 @@ public class UsersController(IUserService userService) : Controller
             return BadRequest(e.Message);
         }
     }
-    
+
+    /// <summary>
+    /// Updates an existing user.
+    /// </summary>
+    /// <param name="id">The ID of the user to update.</param>
+    /// <param name="user">The updated user data.</param>
+    /// <returns>No content if the update is successful.</returns>
     [Authorize]
     [HttpPut("{id}")]
     public async Task<IActionResult> PutUser(int id, UserDto user)
@@ -85,7 +113,12 @@ public class UsersController(IUserService userService) : Controller
             return BadRequest(e.Message);
         }
     }
-    
+
+    /// <summary>
+    /// Deletes a user by ID.
+    /// </summary>
+    /// <param name="id">The ID of the user to delete.</param>
+    /// <returns>No content if the deletion is successful.</returns>
     [Authorize]
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteUser(int id)
@@ -99,16 +132,20 @@ public class UsersController(IUserService userService) : Controller
         {
             return BadRequest(e.Message);
         }
-        
     }
-    
+
+    /// <summary>
+    /// Authenticates a user and returns a JWT token.
+    /// </summary>
+    /// <param name="user">The user login data.</param>
+    /// <returns>A JWT token if the login is successful.</returns>
     [HttpPost("login")]
     public async Task<ActionResult> Login(UserDtoLogin user)
     {
         try
         {
             var result = await userService.LoginUserAsync(user) ?? throw new Exception("User not found");
-            return Ok(new {token = result});
+            return Ok(new { token = result });
         }
         catch (Exception e)
         {
