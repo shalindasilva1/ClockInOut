dfa35cab32d954ebae31839c45b01a0201222852
Shalinda Silva <shalindasilva1@gmail.com>
2025-01-26 20:05:59 +0100

    add postgress db

--- ClockInOut.AppHost/Program.cs
+++ ClockInOut.AppHost/Program.cs
@@ -2,6 +2,14 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var username = builder.AddParameter("username", secret: true);
+var password = builder.AddParameter("password", secret: true);
+
+var postgres = builder.AddPostgres("postgres", username, password)
+    .WithDataVolume(isReadOnly: false)
+    .WithPgAdmin();
+var clockInOutDb = postgres.AddDatabase("ClockInOutDB");
+
 builder.AddProject<ClockAPI>("ClockAPI");
 builder.AddProject<UserAPI>("UserAPI");
 builder.AddProject<TeamAPI>("TeamAPI");
