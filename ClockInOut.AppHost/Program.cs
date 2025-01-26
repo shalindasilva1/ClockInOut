d45dbf305157b3b76c6cd2b6d73dd34ae6fe256c
Shalinda Silva <shalindasilva1@gmail.com>
2025-01-26 19:37:23 +0100

    Add existing projects to app host

--- ClockInOut.AppHost/Program.cs
+++ ClockInOut.AppHost/Program.cs
@@ -1,3 +1,9 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddProject<ClockAPI>("ClockAPI");
+builder.AddProject<UserAPI>("UserAPI");
+builder.AddProject<TeamAPI>("TeamAPI");
 
 builder.Build().Run();
